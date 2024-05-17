e438feceb028c8ab256aebcd0f9c6fab79ed6707
Wei Cheng <wei.cheng@duke.edu>
2024-05-17 14:54:07 -0400

    fix-Conversion from fp32 to bfloat16 is wrong
    - Simply chop off extra bits is enough for the conversion

--- py/fp32tobf16.py
+++ py/fp32tobf16.py
@@ -4,22 +4,9 @@ import struct
 def fp32tobf16(x):
     # Convert float to int representation
     bit_rep = struct.unpack('I', struct.pack('f', x))[0]
-
-    exp = bit_rep & 0x7F800000  # 8-bit exponent field
-    man = bit_rep & 0x007FFFFF  # 7-bit mantissa field
-
-    if exp == 0 and man == 0:
-        # zero
-        return (x & 0xffff0000)
-
-    if exp == 0x7F800000:
-        # infinity or NaNs
-        return (x & 0xffff0000)
-
     bit_rep &= 0xffff0000
     y = struct.unpack('f', struct.pack('I', bit_rep))[
         0]  # Convert int back to float
-
     return y
 
 
