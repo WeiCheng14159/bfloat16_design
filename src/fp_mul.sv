cc5a56290b9ccfe8a2fbb08914af905c787247d3
Wei Cheng <wei.cheng@duke.edu>
2024-05-17 14:37:03 -0400

    fix-Error in fp mul module

--- src/fp_mul.sv
+++ src/fp_mul.sv
@@ -10,7 +10,7 @@ module fp_mul #(
   logic [EXP_WIDTH:0] adder_res;  // EXP_WIDTH + 1 to handle overflow
   logic [EXP_WIDTH-1:0] adder_out;
   logic mul_carry_in;
-  logic [(2*FRAC_WIDTH):0] mul_tmp;  // 2*FRAC_WIDTH + 1 for multiplication result
+  logic [(2*(FRAC_WIDTH+1))-1:0] mul_tmp;  // 2*(FRAC_WIDTH+1) for multiplication result
   logic [FRAC_WIDTH:0] mul_norm_res;  // FRAC_WIDTH + 1 to handle normalization
 
   // exp_adder
@@ -20,12 +20,12 @@ module fp_mul #(
 
   // frac_mul
   assign mul_tmp = {1'b1, op_intf.op1_frac} * {1'b1, op_intf.op2_frac};
-  assign mul_carry_in = mul_tmp[2*FRAC_WIDTH];  // Carry out
+  assign mul_carry_in = mul_tmp[2*FRAC_WIDTH+1];  // Carry out
   assign mul_norm_res = (mul_carry_in) ? mul_tmp[2*FRAC_WIDTH:FRAC_WIDTH] : mul_tmp[2*FRAC_WIDTH-1:FRAC_WIDTH-1];
 
   // fp_mul interface logic
   assign op_intf.op3_sign = op_intf.op1_sign ^ op_intf.op2_sign;
   assign op_intf.op3_exp = (op_intf.overflow) ? {(EXP_WIDTH) {1'b1}} : adder_out;
-  assign op_intf.op3_frac = mul_norm_res[FRAC_WIDTH-1:0];
+  assign op_intf.op3_frac = mul_norm_res[FRAC_WIDTH:1]  /* + mul_norm_res[0]*/;
 
 endmodule
