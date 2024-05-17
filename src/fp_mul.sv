782d5826a10f41609a3c13222f684e588f7adef5
Wei Cheng <wei.cheng@duke.edu>
2024-05-17 12:37:05 -0400

    feat-Combine all multiplier related modules into a single file

--- src/fp_mul.sv
+++ src/fp_mul.sv
@@ -6,28 +6,25 @@ module fp_mul
     op_intf.comp_side op_intf
 );
 
-  logic exp_overflow;
-  logic [7:0] exp_res;
-  logic [6:0] frac_res;
-  logic frac_carry_in;
+  logic [8:0] adder_res;
+  logic [7:0] adder_out;
+  logic mul_carry_in;
+  logic [15:0] mul_tmp;
+  logic [7:0] mul_norm_res;
 
+  // exp_adder
+  assign adder_res = {1'b0, op_intf.op1_exp} + {1'b0, op_intf.op2_exp} - 7'd127;
+  assign op_intf.overflow = adder_res[8];
+  assign adder_out = (mul_carry_in) ? (adder_res[7:0] + 1) : adder_res[7:0];
+
+  // frac_mul
+  assign mul_tmp = {1'b1, op_intf.op1_frac} * {1'b1, op_intf.op2_frac};
+  assign mul_carry_in = mul_tmp[15];
+  assign mul_norm_res = (mul_carry_in) ? mul_tmp[14:7] : mul_tmp[13:6];
+
+  // fp_mul interface logic
   assign op_intf.op3_sign = op_intf.op1_sign ^ op_intf.op2_sign;
-  assign op_intf.op3_exp  = (exp_overflow) ? 8'hFF : exp_res;
-  assign op_intf.overflow = exp_overflow;
-
-  exp_adder u_exp_adder (
-      .in1_exp (op_intf.op1_exp),
-      .in2_exp (op_intf.op2_exp),
-      .out_exp (exp_res),
-      .carry_in(frac_carry_in),
-      .overflow(exp_overflow)
-  );
-
-  frac_mul u_frac_mul (
-      .in1_frac(op_intf.op1_frac),
-      .in2_frac(op_intf.op2_frac),
-      .out_frac(op_intf.op3_frac),
-      .carry_in(frac_carry_in)
-  );
+  assign op_intf.op3_exp = (op_intf.overflow) ? 8'hFF : adder_out;
+  assign op_intf.op3_frac = mul_norm_res[7:1]  /* + normalized_res[0]*/;
 
 endmodule
