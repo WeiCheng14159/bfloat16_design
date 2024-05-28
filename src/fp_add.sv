2bd73634dd1ec9e323e77c6efdacc7187d6a8690
Wei Cheng <wei.cheng@duke.edu>
2024-05-28 15:19:59 -0400

    fix-Remove unsupported systemverilog feature

--- src/fp_add.sv
+++ src/fp_add.sv
@@ -23,32 +23,32 @@ module fp_add #(
   logic same_sign, frac_add_overflow;
   logic [$clog2(FRAC_WIDTH+2)-1:0] clz_count;
 
-  always_comb begin
-    if (op_intf.op1_exp > op_intf.op2_exp) begin
-      exp_diff = op_intf.op1_exp - op_intf.op2_exp;
-      exp_aligned = op_intf.op1_exp;
-      frac1_aligned = {1'b1, op_intf.op1_frac};
-      frac2_aligned = {1'b1, op_intf.op2_frac} >> exp_diff;
+  always @(*) begin
+    if (op1_exp > op2_exp) begin
+      exp_diff = op1_exp - op2_exp;
+      exp_aligned = op1_exp;
+      frac1_aligned = {1'b1, op1_frac};
+      frac2_aligned = {1'b1, op2_frac} >> exp_diff;
     end else begin
-      exp_diff = op_intf.op2_exp - op_intf.op1_exp;
-      exp_aligned = op_intf.op2_exp;
-      frac1_aligned = {1'b1, op_intf.op1_frac} >> exp_diff;
-      frac2_aligned = {1'b1, op_intf.op2_frac};
+      exp_diff = op2_exp - op1_exp;
+      exp_aligned = op2_exp;
+      frac1_aligned = {1'b1, op1_frac} >> exp_diff;
+      frac2_aligned = {1'b1, op2_frac};
     end
   end
 
-  assign same_sign = (op_intf.op1_sign == op_intf.op2_sign);
-  always_comb begin
+  assign same_sign = (op1_sign == op2_sign);
+  always @(*) begin
     if (same_sign) begin
       frac_sum = frac1_aligned + frac2_aligned;
-      op_intf.op3_sign = op_intf.op1_sign;
+      op3_sign = op1_sign;
     end else begin
       if (frac1_aligned > frac2_aligned) begin
         frac_sum = frac1_aligned - frac2_aligned;
-        op_intf.op3_sign = op_intf.op1_sign;
+        op3_sign = op1_sign;
       end else begin
         frac_sum = frac2_aligned - frac1_aligned;
-        op_intf.op3_sign = op_intf.op2_sign;
+        op3_sign = op2_sign;
       end
     end
   end
@@ -61,18 +61,18 @@ module fp_add #(
   );
 
   assign frac_add_overflow = frac_sum[FRAC_WIDTH+1];
-  always_comb begin
+  always @(*) begin
     if (same_sign) begin
-      op_intf.op3_frac = frac_add_overflow ? frac_sum[FRAC_WIDTH:1] : frac_sum[FRAC_WIDTH-1:0];
-      op_intf.op3_exp  = frac_add_overflow ? exp_aligned + 1 : exp_aligned;
+      op3_frac = frac_add_overflow ? frac_sum[FRAC_WIDTH:1] : frac_sum[FRAC_WIDTH-1:0];
+      op3_exp  = frac_add_overflow ? exp_aligned + 1 : exp_aligned;
     end else begin  // frac_sum = (frac1 - frac2) or swapped
       if (~|frac_sum) begin
-        op_intf.op3_frac = {FRAC_WIDTH{1'b0}};
-        op_intf.op3_exp  = exp_aligned + 1;
+        op3_frac = {FRAC_WIDTH{1'b0}};
+        op3_exp  = exp_aligned + 1;
       end else begin
         frac_sub = {frac_sum << clz_count};
-        op_intf.op3_frac = frac_sub[FRAC_WIDTH-1:0];
-        op_intf.op3_exp = exp_aligned + clz_count;
+        op3_frac = frac_sub[FRAC_WIDTH-1:0];
+        op3_exp  = exp_aligned + clz_count;
       end
     end
   end
@@ -87,11 +87,11 @@ module clz_for_frac #(
 );
 
   integer i;
-  always_comb begin
+  always @(*) begin
     count = 0;
     for (i = N - 1; i >= 0; i = i - 1) begin
       if (data[i] == 1'b0) count = count + 1;
-      else break;
+      else count = count;
     end
   end
 
