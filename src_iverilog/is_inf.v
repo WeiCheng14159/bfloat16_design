d3c873b5d6d05d7fb74dbec6e6ffb5563f376ea0
Yi-Chyun Wong <yw480@duke.edu>
2024-05-31 00:12:25 -0400

    Added module to identify infinities of both signs

---
+++ src_iverilog/is_inf.v
@@ -0,0 +1,16 @@
+module is_NaN (num, is_inf);
+    parameter DATA_WIDTH = 16;
+    parameter EXP_WIDTH = 8;
+    parameter FRAC_WIDTH = 7;
+
+    input [DATA_WIDTH-1:0] num;
+    output is_inf;
+
+    wire [EXP_WIDTH-1:0] exp;
+    wire [FRAC_WIDTH-1:0] frac;
+
+    assign exp = num[DATA_WIDTH-2:DATA_WIDTH-1-EXP_WIDTH];
+    assign frac = num[FRAC_WIDTH-1:0];
+
+    assign is_inf = (&exp) & !(|frac);
+endmodule
