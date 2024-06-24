7ca37580491b8acd653447f92ceb5f7074e61613
Yi-Chyun Wong <yw480@duke.edu>
2024-06-24 17:56:56 -0400

    Reverted bfloat16 back to original constructor; changed fp32tobf16 to incorporate initialisation of bfloat16 using strings

--- py/bfloat16.py
+++ py/bfloat16.py
@@ -4,32 +4,13 @@ import struct
 
 class bfloat16:
     def __init__(self, value):
-        if isinstance(value, float):
-            self.value = fp32tobf16(value)
-            self.bit_rep = struct.unpack('I', struct.pack('f', self.value))[0]
-            self.exp = (self.bit_rep & 0x7F800000) >> 23
-            self.fp32_man = self.bit_rep & 0x007FFFFF
-            self.bf16_man = (self.bit_rep & 0x007F0000) >> 16
-            self.sign = (self.bit_rep & 0x80000000) >> 31
-        elif isinstance(value, str):
-            if value.startswith('0b') and len(value) == 18 and all(c in '01' for c in value[2:]):
-                sign = int(value[2], 2)
-                exp_with_bias = int(value[3:11], 2)
-                mantissa = int(value[11:], 2) / 128.0
-                
-                pos_0 = (sign == 0 and exp_with_bias == 0 and mantissa == 0)
-                neg_0 = (sign == 1 and exp_with_bias == 0 and mantissa == 0)
-                NaN = (exp_with_bias == 255 and mantissa != 0)
+        self.value = fp32tobf16(value)
+        self.bit_rep = struct.unpack('I', struct.pack('f', self.value))[0]
+        self.exp = (self.bit_rep & 0x7F800000) >> 23
+        self.fp32_man = self.bit_rep & 0x007FFFFF
+        self.bf16_man = (self.bit_rep & 0x007F0000) >> 16
+        self.sign = (self.bit_rep & 0x80000000) >> 31
 
-                float_value = float('nan') if NaN else 0.0 if pos_0 else -0.0 if neg_0 else ((-1)**sign) * (1 + mantissa) * (2 ** (exp_with_bias - 127))
-                self.__init__(float_value)
-            elif value.startswith('0x') and len(value) == 6 and all(c in '0123456789abcdefABCDEF' for c in value[2:]):
-                bin_string = bin(int(value, 16))[2:].zfill(16)
-                self.__init__('0b' + bin_string)
-            else:
-                raise ValueError("Invalid input format. Please provide a valid 16-bit binary starting with 0b or a 4-digit hexadecimal number starting with 0x.")
-        else:
-            raise TypeError("Value must be a float, binary string, or hexadecimal string.")
         
     def __add__(self, other):
         if isinstance(other, bfloat16):
--- py/fp32tobf16.py
+++ py/fp32tobf16.py
@@ -2,12 +2,25 @@ import struct
 
 
 def fp32tobf16(x):
-    # Convert float to int representation
-    bit_rep = struct.unpack('I', struct.pack('f', x))[0]
-    bit_rep &= 0xffff0000
-    y = struct.unpack('f', struct.pack('I', bit_rep))[
-        0]  # Convert int back to float
-    return y
+    if isinstance(x, float):
+        bit_rep = struct.unpack('I', struct.pack('f', x))[0]
+        bit_rep &= 0xffff0000
+        y = struct.unpack('f', struct.pack('I', bit_rep))[
+            0]
+        return y
+    if isinstance(x, str):
+        if x.startswith('0b') and len(x) == 18 and all(c in '01' for c in x[2:]):
+            bin_bit_rep = int((x[2:] + '0' * 16), 2)
+            bin_to_float = struct.unpack('f', struct.pack('I', bin_bit_rep))[0]
+            return bin_to_float
+        elif x.startswith('0x') and len(x) == 6 and all(c in '0123456789abcdefABCDEF' for c in x[2:]):
+            hex_bit_rep = int((x[2:] + '0' * 4), 16)
+            hex_to_float = struct.unpack('f', struct.pack('I', hex_bit_rep))[0]
+            return hex_to_float
+        else:
+                raise ValueError("Invalid input format. Please provide a valid 16-bit binary starting with 0b or a 4-digit hexadecimal number starting with 0x.")
+    else:
+        raise TypeError("Value must be a float, binary string, or hexadecimal string.")
 
 
 if __name__ == "__main__":
